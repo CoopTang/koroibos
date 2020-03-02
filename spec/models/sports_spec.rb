0e00415033cc3125c55696327467f7e577ca9789
Michael Cooper <macoop2363@gmail.com>
2020-03-02 10:23:07 -0700

    Add model test for Sport

---
+++ spec/models/sports_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Sport, type: :model do
+  describe 'attributes' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+    it { should have_many :olympians }
+  end
+
+  describe 'methods' do
+
+  end
+end
