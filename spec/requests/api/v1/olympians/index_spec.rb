4827fa0ebcdd92f8a585c376bc26862da4b35468
Michael Cooper <macoop2363@gmail.com>
2020-03-02 19:27:14 -0700

    Fix logical errors in OlympianController test

--- spec/requests/api/v1/olympians/index_spec.rb
+++ spec/requests/api/v1/olympians/index_spec.rb
@@ -7,6 +7,7 @@ describe 'Olympians Index:', type: :request do
       @spain = Team.create!(name: "Spain")
 
       @sport = Sport.create!(name: "Pole Vaulting")
+      @swim  = Sport.create!(name: "Swimming")
       @event = Event.create!(name: "Vauling really high", sport_id: @sport.id)
 
       @michael = Olympian.create!(
@@ -29,38 +30,42 @@ describe 'Olympians Index:', type: :request do
       @michael_sport = SportOlympian.create!(sport_id: @sport.id, olympian_id: @michael.id)
       @veronica_sport = SportOlympian.create!(sport_id: @sport.id, olympian_id: @veronica.id)
 
-      @michael_event = EventOlympian.create!(event_id: @sport.id, olympian_id: @michael.id)
-      @veronica_event = EventOlympian.create!(event_id: @sport.id, olympian_id: @veronica.id)
+      @michael_event = EventOlympian.create!(event_id: @event.id, olympian_id: @michael.id)
+      @veronica_event = EventOlympian.create!(event_id: @event.id, olympian_id: @veronica.id)
 
       @medalist = Medalist.create!(event_id: @event.id, olympian_id: @veronica.id, medal: 0)
     end
     it 'I get a JSON response with all olympians in the database', :vcr do
       get '/api/v1/olympians'
 
+      response = JSON.parse(@response.body, symbolize_names: true)
+      response[:olympians] = response[:olympians].sort_by { |olympian| olympian[:name] }
+
       expect(response).to have_key(:olympians)
       expect(response[:olympians]).to be_an(Array)
-      expect(response[:olympians].length).to be_an(2)
+      expect(response[:olympians].length).to eq(2)
+
       expect(response[:olympians][0]).to have_key(:name)
-      expect(response[:olympians][0].name).to eq('Michael')
+      expect(response[:olympians][0][:name]).to eq('Michael')
       expect(response[:olympians][0]).to have_key(:team)
-      expect(response[:olympians][0].team).to eq('US')
+      expect(response[:olympians][0][:team]).to eq('US')
       expect(response[:olympians][0]).to have_key(:age)
-      expect(response[:olympians][0].age).to eq(29)
+      expect(response[:olympians][0][:age]).to eq(29)
       expect(response[:olympians][0]).to have_key(:sport)
-      expect(response[:olympians][0].sport).to eq('Pole Vaulting')
+      expect(response[:olympians][0][:sport]).to eq('Pole Vaulting')
       expect(response[:olympians][0]).to have_key(:total_medals_won)
-      expect(response[:olympians][0].total_medals_won).to eq(0)
+      expect(response[:olympians][0][:total_medals_won]).to eq(0)
 
       expect(response[:olympians][1]).to have_key(:name)
-      expect(response[:olympians][1].name).to eq('Veronica')
+      expect(response[:olympians][1][:name]).to eq('Veronica')
       expect(response[:olympians][1]).to have_key(:team)
-      expect(response[:olympians][1].team).to eq('Spain')
+      expect(response[:olympians][1][:team]).to eq('Spain')
       expect(response[:olympians][1]).to have_key(:age)
-      expect(response[:olympians][1].age).to eq(32)
+      expect(response[:olympians][1][:age]).to eq(32)
       expect(response[:olympians][1]).to have_key(:sport)
-      expect(response[:olympians][1].sport).to eq('Pole Vaulting')
+      expect(response[:olympians][1][:sport]).to eq('Pole Vaulting')
       expect(response[:olympians][1]).to have_key(:total_medals_won)
-      expect(response[:olympians][1].total_medals_won).to eq(1)
+      expect(response[:olympians][1][:total_medals_won]).to eq(1)
     end
   end
 end
